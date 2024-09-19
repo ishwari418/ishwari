178b46bcc847d0e629d8e770e188457cd9b0c9c6
ishwari418 <ishwaribelhekar29@gmail.com>
2024-09-18 23:23:28 -0700

    Add files via upload

---
+++ CV Writing.bat
@@ -0,0 +1,5 @@
+@ echo off
+"C:\Program Files (x86)\WinCDEmu\batchmnt64.exe" "d:\English\Focus_TYCVWS\Focus_TYCVWS.iso" X:
+d:\English\Scripts\openprog.vbs """C:\Program Files (x86)\Focus Multimedia\CV Writing Skills\Teaching-you CV Writing Skills.exe""" "C:\Program Files (x86)\Focus Multimedia\CV Writing Skills\"
+
+
